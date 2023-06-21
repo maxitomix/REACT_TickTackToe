57c2561ca3697bfc962a92bb3dd69919beb1506a
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-06-21 15:17:04 -0400

    done

--- src/App.js
+++ src/App.js
@@ -12,11 +12,56 @@ function Square({value, onSquareClick}) {
   );
 }
 
+export default function Game() {
+  // const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares =  history[currentMove];
 
-export default function Board() {
+  function handlePlay(nextSquares) {   
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+    // setXIsNext(!xIsNext);
 
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    // setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i){
 
@@ -31,8 +76,7 @@ export default function Board() {
       nextSquares[i] = 'O';
     }
 
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -85,3 +129,4 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
